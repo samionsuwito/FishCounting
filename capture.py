93617358ea471cc7f4ccc2bad02bf908e86db481
isitusnoamow <240135@hkis.edu.hk>
2022-10-08 22:09:06 +0800

    camera

---
+++ capture.py
@@ -0,0 +1,37 @@
+import os
+from picamera2 import Picamera2
+import time
+import csv
+
+pretime = 0
+newFolder = True
+folderN = 1
+num = 1
+
+cam = Picamera2()
+
+while newFolder:
+    if os.path.exists(f"trials/trial{folderN}"):
+        folderN += 1
+    else:
+        os.mkdir(f"trials/trial{folderN}")
+        os.mkdir(f"trials/trial{folderN}/images")
+        os.mkdir(f"trials/trial{folderN}/annotated")
+        newFolder = False
+
+with open(f"trials/trial{folderN}/data.csv",'w') as csvfile:
+    fieldnames = ['ID','Time', 'Picture','Fishes']
+    filewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    filewriter.writeheader()
+    while True:
+        if(pretime + 10 <= time.time()):
+            pretime = time.time()
+            cam.start_and_capture_file(f"trials/trial{folderN}/images/{num}.jpg")
+            filewriter.writerow({'ID':num,'Time':time.asctime(time.localtime()),'Picture':f"trials/trial{folderN}/images/{num}",'Fishes':0})
+            num += 1
+
+
+
+
+
+        
