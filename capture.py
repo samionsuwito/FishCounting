f4da283370c9377df0dde68eaf9fc6bfd21efe6f
isitusnoamow <240135@hkis.edu.hk>
2022-10-11 22:27:27 +0800

    adjusted

--- capture.py
+++ capture.py
@@ -29,16 +29,17 @@ with open(f"trials/trial{folderN}/data.csv",'w') as csvfile:
     while True:
         if(pretime + 10 <= time.time()):
             pretime = time.time()
-            cam.capture_file(f"trials/trial{folderN}/images/{num}.jpg")
+            #cam.capture_file(f"trials/trial{folderN}/images/{num}.jpg")
+            img = cam.capture_array()
             #contrast part
-            img = cv2.imread(f"trials/trial{folderN}/images/{num}.jpg", 1)
+            #img = cv2.imread(f"trials/trial{folderN}/images/{num}.jpg", 1)
             l = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
             l_c, a, b = cv2.split(lab)
             clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(8,8))
             cl = clahe.apply(l_c)
             limg = cv2.merge((cl,a,b))
             final_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-            cv2.imwrite('contrast_photo.jpg', final_img)
+            cv2.imwrite(f"trials/trial{folderN}/images/{num}.jpg", final_img)
             
             filewriter.writerow({'ID':num,'Time':time.asctime(time.localtime()),'Picture':'contrast_photo.jpg','Fishes':0})
             num += 1
